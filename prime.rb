79171b07b9a8e29d9c35923e1e561f7cbbc0406f
danny galimi <dannygalimi13@aim.com>
2022-06-02 09:49:26 -0400

    commit tree 270326161d137785c8a95e8e3c4b48c016a0e2ca

--- prime.rb
+++ prime.rb
@@ -1,1 +1,27 @@
-# Add  code here!
+def prime?(integer)
+    if integer == 0 || integer < 0 || integer == 1 
+        false
+    else
+        (2..integer-1).to_a.all? do |factor|
+            integer % factor != 0
+        end 
+    end 
+end 
+
+# def prime?(integer)
+#     if integer % 1 == 0 && integer % integer == 0 && integer >= 1
+#         true
+#     else
+#         false
+#     end 
+# end 
+
+# def prime?(num)
+#     if num < 0 or num == 0 or num == 1
+#       return false
+#     else
+#       (2..num-1).to_a.all? do |possible_factor|
+#         num % possible_factor != 0
+#       end
+#     end
+#   end
